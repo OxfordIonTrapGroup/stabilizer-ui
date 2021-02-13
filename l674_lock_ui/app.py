0158d03aaa7fe83ab272531dc694f258b478cddb
David Nadlinger <code@klickverbot.at>
2021-02-13 01:10:51 +0000

    Link sliders/spin boxes

--- l674_lock_ui/app.py
+++ l674_lock_ui/app.py
@@ -1,6 +1,10 @@
-from PyQt5 import QtWidgets, uic
+import asyncio
 import os
+from PyQt5 import QtWidgets, uic
+from qasync import QEventLoop
 import sys
+
+from .ui_utils import link_slider_to_spinbox
 
 
 class UI(QtWidgets.QMainWindow):
@@ -10,9 +14,22 @@ class UI(QtWidgets.QMainWindow):
         ui_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "main.ui")
         uic.loadUi(ui_path, self)
 
+        self._link_paired_widgets()
+
+    def _link_paired_widgets(self):
+        for s, b in [(self.fastPGainSlider, self.fastPGainBox),
+                     (self.fastIGainSlider, self.fastIGainBox),
+                     (self.notchFreqSlider, self.notchFreqBox),
+                     (self.notchQSlider, self.notchQBox),
+                     (self.slowPGainSlider, self.slowPGainBox),
+                     (self.slowIGainSlider, self.slowIGainBox)]:
+            link_slider_to_spinbox(s, b)
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
     ui = UI()
     ui.show()
     app.exec_()
