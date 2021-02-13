0fc72886b56b615d4d2c2dabc44f38089a7b8b34
David Nadlinger <code@klickverbot.at>
2021-02-13 10:27:23 +0000

    Update Stabilizer on UI changes

--- l674_lock_ui/app.py
+++ l674_lock_ui/app.py
@@ -6,6 +6,7 @@ from PyQt5 import QtCore, QtWidgets, uic
 from qasync import QEventLoop
 import sys
 
+from .iir import *
 from .ui_utils import link_slider_to_spinbox
 
 logger = logging.getLogger(__name__)
@@ -80,34 +81,153 @@ class UI(QtWidgets.QMainWindow):
         super().closeEvent(event)
 
 
+class StabilizerError(Exception):
+    pass
+
+
 async def stabilizer_task(ui: UI, stabilizer_host, stabilizer_port=1235):
     try:
         reader, writer = await asyncio.open_connection(stabilizer_host, stabilizer_port)
 
-        def update_fast_iir():
-            pass
+        async def query(msg):
+            s = json.dumps(msg, separators=[",", ":"]).replace('"', "'")
+            assert "\n" not in s
+            self.writer.write((s + "\n").encode("ascii"))
+            logger.debug("[stabilizer] Sent: %s", s)
 
-        def update_notch():
-            pass
+            r = (await self.reader.readline()).decode()
+            logger.debug("[stabilizer] Recv: %s", r)
 
-        def update_slow_iir():
-            pass
+            ret = json.loads(r)
+            if ret["code"] != 200:
+                raise StabilizerError(ret)
+            return ret
 
-        def update_adc1_mode():
-            pass
+        async def update_biquad(name, coeffs):
+            await query({
+                "req": "Write",
+                "attribute": f"stabilizer/iir{name}/state",
+                "value": {
+                    "channel": 0,  # TODO: Meaning unclear; currently not intepreted…
+                    "iir": iir_config(coeffs)
+                }
+            })
 
-        update_fast_iir()
-        update_notch()
-        update_slow_iir()
-        update_adc1_mode()
+        async def update_fast_iir():
+            if ui.disablePztButton.isChecked():
+                kp = ki = 0
+            elif ui.rampPztButton.isChecked():
+                kp = 1
+                ki = 0
+            elif ui.enablePztButton.isChecked():
+                kp = ui.fastPGainBox.value()
+                ki = ui.fastIGainBox.value() * 1e3
+            else:
+                assert False
+            await update_biquad("0", pi_coeffs(kp=kp, ki=ki))
+
+        async def update_notch():
+            if ui.notchGroup.isChecked():
+                coeffs = notch_coeffs(ui.notchFreqBox.value() * 1e3,
+                                      ui.notchQBox.value())
+            else:
+                # Pass through.
+                coeffs = pi_coeffs(kp=1.0, ki=0.0)
+            await update_biquad("0_b", coeffs)
+
+        async def update_slow_iir():
+            if ui.enablePztButton.isChecked() and self.slowPIDGroup.isChecked():
+                kp = ui.slowPGainBox.value()
+                ki = ui.slowIGainBox.value() * 1e3
+            else:
+                kp = ki = 0
+            await update_biquad("1", pi_coeffs(kp=kp, ki=ki))
+
+        async def update_adc1_mode():
+            if self.adc1IgnoreButton.isChecked():
+                value = "ignore"
+            elif self.adc1FastInputButton.isChecked():
+                value = "iir0_input"
+            elif self.adc1FastOutputButton.isChecked():
+                value = "iir0_b_input"
+            else:
+                assert False
+            await query({
+                "req": "Write",
+                "attribute": "stabilizer/route_adc1",
+                "value": value
+            })
+
+        # Query in most glitchless order.
+        await update_adc1_mode()
+        await update_notch()
+        await update_fast_iir()
+        await update_slow_iir()
+
+        fast_iir_changed = asyncio.Event()
+        notch_changed = asyncio.Event()
+        slow_iir_changed = asyncio.Event()
+        adc1_changed = asyncio.Event()
+
+        pid_state_widgets = [ui.disablePztButton, ui.rampPztButton, ui.enablePztButton]
+        links = {
+            fast_iir_changed: [
+                ui.fastPGainBox,
+                ui.fastIGainBox,
+            ] + pid_state_widgets,
+            notch_changed: [
+                ui.notchBox,
+                ui.notchFreqBox,
+                ui.notchQBox,
+            ],
+            slow_iir_changed: [
+                ui.slowPGainBox,
+                ui.slowIGainBox,
+                ui.slowPIDGroup,
+            ] + pid_state_widgets,
+            adc1_changed: [
+                ui.adc1IgnoreButton,
+                ui.adc1FastInputButton,
+                ui.adc1FastOutputButton,
+            ]
+        }
+        for event, widgets in links:
+            # Capture loop variable.
+            def set_ev():
+                event.set()
+
+            for widget in widgets:
+                if hasattr(widget, "valueChanged"):
+                    widget.valueChanged.connect(set_ev)
+                elif hasattr(widget, "toggled"):
+                    widget.toggled.connect(set_ev)
+                else:
+                    assert False
 
         ui.pztLockGroup.setEnabled(True)
+
+        while True:
+            done_tasks, _ = asyncio.wait([event.wait() for event in links.keys()],
+                                         timeout=1.0,
+                                         return_when=asyncio.FIRST_COMPLETED)
+            if adc1_changed.is_set():
+                await update_adc1_mode()
+            if notch_changed.is_set():
+                await update_notch()
+            if fast_iir_changed.is_set():
+                await update_fast_iir()
+            if slow_iir_changed.is_set():
+                await update_slow_iir()
+            if not done_tasks:
+                # TODO: Ping.
+                pass
 
     except Exception as e:
         if isinstance(e, asyncio.CancelledError):
             return
         logger.exception("Failure in Stabilizer communication task")
         ui.pztLockGroup.setEnabled(False)
+        # Set main window status line?
         raise
 
 
---
+++ l674_lock_ui/iir.py
@@ -0,0 +1,48 @@
+"""
+Derive Stabilizer IIR config dicts from filter definitions in physical units.
+
+(PID math based on the stabilizer.py script from the Stabilizer repository.)
+"""
+
+from typing import List
+from scipy.signal.filter_design import iirnotch
+import numpy as np
+
+T_CYCLE = 2e-6
+FULL_SCALE = float((1 << 15) - 1)
+
+
+def iir_config(biquad_coeffs: List[float], x_offset: float = 0.0):
+    ba = np.array(biquad_coeffs, dtype=np.float)
+    dc_gain = ba[:3].sum()
+    return {
+        "ba": ba,
+        "y_min": -FULL_SCALE - 1,
+        "y_max": FULL_SCALE,
+        "y_offset": dc_gain * FULL_SCALE * x_offset
+    }
+
+
+def notch_coeffs(freq: float, q: float) -> List[float]:
+    b, a = iirnotch(freq, q, fs=(1 / T_CYCLE))
+    return np.concatenate(b, a[1:])
+
+
+def pi_coeffs(kp: float, ki: float, gain_limit: float = 0.0) -> List[float]:
+    ki = np.copysign(ki, kp) * T_CYCLE * 2
+    eps = np.finfo(np.float32).eps
+    if abs(ki) < eps:
+        return [kp, 0.0, 0.0, 0.0, 0.0]
+
+    gain_limit = np.copysign(gain_limit, kp)
+    if abs(gain_limit) < eps:
+        c = 1.0
+    else:
+        c = 1.0 / (1.0 + ki / gain_limit)
+    a1 = 2 * c - 1.0
+    b0 = ki * c + kp
+    b1 = ki * c - a1 * kp
+    if abs(b0 + b1) < eps:
+        raise ValueError("low integrator gain and/or gain limit")
+
+    return [b0, b1, 0.0, a1, 0.0]
