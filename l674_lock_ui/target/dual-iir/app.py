fd7279fcfe19222019834ca53acad5a7e03ec933
Dougal Main <dougal.main@physics.ox.ac.uk>
2022-09-01 17:04:02 +0100

    Fix some small issues with channel settings.

--- l674_lock_ui/target/dual-iir/app.py
+++ l674_lock_ui/target/dual-iir/app.py
@@ -9,6 +9,7 @@ from qasync import QEventLoop
 import sys
 
 from ...channel_settings import ChannelSettings
+from ...stream.fft_scope import FftScope
 
 logger = logging.getLogger(__name__)
 
@@ -17,16 +18,27 @@ class UI(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
 
+        wid = QtWidgets.QWidget(self)
+        self.setCentralWidget(wid)
+        layout = QtWidgets.QHBoxLayout()
+
+        # Create UI for channel settings.
         self.channel_settings = [
         ('Channel 0', ChannelSettings()),
         ('Channel 1', ChannelSettings())
         ]
 
-        tab_channel_settings = QtWidgets.QTabWidget()
+        self.tab_channel_settings = QtWidgets.QTabWidget()
         for label, channel in self.channel_settings:
-            tab_channel_settings.addTab(channel, label)
+            self.tab_channel_settings.addTab(channel, label)
+        layout.addWidget(self.tab_channel_settings)
 
-        self.setCentralWidget(tab_channel_settings)
+        # Create UI for FFT scope.
+        self.fft_scope = FftScope()
+        layout.addWidget(self.fft_scope)
+
+        # Set main window layout
+        wid.setLayout(layout)
 
 
 def main():
@@ -42,6 +54,7 @@ def main():
         asyncio.set_event_loop(loop)
 
         ui = UI()
+        ui.resize(1200, 600)
         ui.show()
 
         sys.exit(loop.run_forever())
