82c39160224fceff5e0882a8c02cee09ef05e096
Dougal Main <dougal.main@physics.ox.ac.uk>
2022-09-08 18:00:58 +0100

    Refactored wrapper for stabilizer package.

--- l674_lock_ui/iir.py
+++ l674_lock_ui/iir.py
@@ -1,8 +1,6 @@
 from enum import Enum
 import stabilizer.iir_coefficients as iir
 from stabilizer import SAMPLE_PERIOD
-from collections import namedtuple
-from math import inf
 
 
 class _AbstractArgs:
@@ -12,53 +10,43 @@ class _AbstractArgs:
                 raise ValueError(f"Key: {key} not found.")
             setattr(self, key, value)
 
+    @classmethod
+    def get_coefficients(cls, *args, **kwargs):
+        args = cls(*args, **kwargs)
+        args.sample_period = SAMPLE_PERIOD
+        return cls.coefficients_func(args)
 
-class XPassArgs(_AbstractArgs):
+
+class LowpassArgs(_AbstractArgs):
+    filter_type = "lowpass"
     parameters = ["f0", "K"]
+    coefficients_func = iir.lowpass_coefficients
+
+
+class HighpassArgs(_AbstractArgs):
+    filter_type = "highpass"
+    parameters = ["f0", "K"]
+    coefficients_func = iir.highpass_coefficients
+
+
+class AllpassArgs(_AbstractArgs):
+    filter_type = "allpass"
+    parameters = ["f0", "K"]
+    coefficients_func = iir.allpass_coefficients
 
 
 class NotchArgs(_AbstractArgs):
+    filter_type = "notch"
     parameters = ["f0", "Q", "K"]
+    coefficients_func = iir.notch_coefficients
 
 
 class PidArgs(_AbstractArgs):
+    filter_type = "pid"
     parameters = [
         "Kp", "Ki", "Ki_limit", "Kii", "Kii_limit", "Kd", "Kd_limit", "Kdd", "Kdd_limit"
     ]
+    coefficients_func = iir.pid_coefficients
 
 
-def get_lowpass_coefficients(*args, **kwargs):
-    args = XPassArgs(*args, **kwargs)
-    args.sample_period = SAMPLE_PERIOD
-    return iir.lowpass_coefficients(args)
-
-
-def get_highpass_coefficients(*args, **kwargs):
-    args = XPassArgs(*args, **kwargs)
-    args.sample_period = SAMPLE_PERIOD
-    return iir.highpass_coefficients(args)
-
-
-def get_allpass_coefficients(*args, **kwargs):
-    args = XPassArgs(*args, **kwargs)
-    args.sample_period = SAMPLE_PERIOD
-    return iir.allpass_coefficients(args)
-
-
-def get_notch_coefficients(*args, **kwargs):
-    args = NotchArgs(*args, **kwargs)
-    args.sample_period = SAMPLE_PERIOD
-    return iir.notch_coefficients(args)
-
-
-def get_pid_coefficients(*args, **kwargs):
-    args = PidArgs(*args, **kwargs)
-    args.sample_period = SAMPLE_PERIOD
-    return iir.pid_coefficients(args)
-
-
-__all__ = [
-    "get_lowpass_coefficients", "get_highpass_coefficients", "get_allpass_coefficients",
-    "get_notch_coefficients", "get_pid_coefficients", "XPassArgs", "NotchArgs",
-    "PidArgs"
-]
+FILTERS = [PidArgs, NotchArgs, LowpassArgs, HighpassArgs, AllpassArgs]
