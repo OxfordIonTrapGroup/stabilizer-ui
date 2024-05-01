0f9eab8c3f6488544629cacca5ed6cefd52657fb
ayushagrawal <ayush.agrawal6278@gmail.com>
2024-05-01 23:09:16 +0100

    Move Dual_IIR ui into separate file

--- stabilizer_ui/target/dual_iir/app.py
+++ stabilizer_ui/target/dual_iir/app.py
@@ -9,17 +9,15 @@ from PyQt5 import QtWidgets
 from qasync import QEventLoop
 from stabilizer.stream import get_local_ip, Parser, AdcDecoder, DacDecoder
 
+from .ui import UiWindow
 from .interface import StabilizerInterface
 
 from ...mqtt import MqttInterface
-from ...iir.channel_settings import ChannelSettings
-from ...stream.fft_scope import FftScope
 from ...stream.thread import StreamThread
 from ...ui_mqtt_bridge import NetworkAddress, UiMqttConfig, UiMqttBridge
 from ... import ui_mqtt_bridge
 from ...ui_utils import fmt_mac
 from ...iir.filters import FILTERS
-from ...widgets.ui import AbstractUiWindow
 
 logger = logging.getLogger(__name__)
 
@@ -31,54 +29,8 @@ DEFAULT_WINDOW_SIZE = (1200, 600)
 parser = Parser([AdcDecoder(), DacDecoder()])
 
 
-class UI(AbstractUiWindow):
-
-    def __init__(self, title: str = "Dual IIR"):
-        super().__init__()
-
-        self.setWindowTitle(title)
-
-        wid = QtWidgets.QWidget(self)
-        self.setCentralWidget(wid)
-        layout = QtWidgets.QHBoxLayout()
-
-        # Create UI for channel settings.
-        self.channel_settings = [ChannelSettings(), ChannelSettings()]
-
-        self.tab_channel_settings = QtWidgets.QTabWidget()
-        for i, channel in enumerate(self.channel_settings):
-            self.tab_channel_settings.addTab(channel, f"Channel {i}")
-        layout.addWidget(self.tab_channel_settings)
-
-        # Create UI for FFT scope.
-        self.fft_scope = FftScope(parser)
-        layout.addWidget(self.fft_scope)
-
-        # Set main window layout
-        wid.setLayout(layout)
-
-    def update_stream(self, payload):
-        # if self.tab_channel_settings.currentIndex() != 1:
-        #    return
-        self.fft_scope.update(payload)
-
-    async def update_transfer_function(self, setting, all_values):
-        if setting.split("/")[0] == "ui":
-            channel, iir = setting.split("/")[1:3]
-            path_root = f"ui/{channel}/{iir}/"
-            filter_type = all_values[path_root + "filter"]
-            filter_idx = [f.filter_type for f in FILTERS].index(filter_type)
-            kwargs = {
-                param: all_values[path_root + f"{filter_type}/{param}"]
-                for param in FILTERS[filter_idx].parameters
-            }
-            ba = FILTERS[filter_idx].get_coefficients(**kwargs)
-            _iir_widget = self.channel_settings[int(channel)].iir_widgets[int(iir)]
-            _iir_widget.update_transfer_function(ba)
-
-
 async def update_stabilizer(
-    ui: UI,
+    ui: UiWindow,
     stabilizer_interface: StabilizerInterface,
     root_topic: str,
     broker_address: NetworkAddress,
@@ -214,7 +166,7 @@ def main():
     with QEventLoop(app) as loop:
         asyncio.set_event_loop(loop)
 
-        ui = UI(f"{args.name} [{fmt_mac(args.stabilizer_mac)}]")
+        ui = UiWindow(f"{args.name} [{fmt_mac(args.stabilizer_mac)}]")
         ui.resize(*DEFAULT_WINDOW_SIZE)
         ui.show()
 
---
+++ stabilizer_ui/target/dual_iir/ui.py
@@ -0,0 +1,54 @@
+from PyQt5 import QtWidgets
+from stabilizer.stream import Parser, AdcDecoder, DacDecoder
+
+from ...widgets.ui import AbstractUiWindow
+from ...iir.channel_settings import ChannelSettings
+from ...stream.fft_scope import FftScope
+from ...iir.filters import FILTERS
+
+
+class UiWindow(AbstractUiWindow):
+
+    def __init__(self, title: str = "Dual IIR"):
+        super().__init__()
+
+        self.setWindowTitle(title)
+
+        wid = QtWidgets.QWidget(self)
+        self.setCentralWidget(wid)
+        layout = QtWidgets.QHBoxLayout()
+
+        # Create UI for channel settings.
+        self.channel_settings = [ChannelSettings(), ChannelSettings()]
+
+        self.tab_channel_settings = QtWidgets.QTabWidget()
+        for i, channel in enumerate(self.channel_settings):
+            self.tab_channel_settings.addTab(channel, f"Channel {i}")
+        layout.addWidget(self.tab_channel_settings)
+
+        # Create UI for FFT scope.
+        streamParser = Parser([AdcDecoder(), DacDecoder()])
+        self.fft_scope = FftScope(streamParser)
+        layout.addWidget(self.fft_scope)
+
+        # Set main window layout
+        wid.setLayout(layout)
+
+    def update_stream(self, payload):
+        # if self.tab_channel_settings.currentIndex() != 1:
+        #    return
+        self.fft_scope.update(payload)
+
+    async def update_transfer_function(self, setting, all_values):
+        if setting.split("/")[0] == "ui":
+            channel, iir = setting.split("/")[1:3]
+            path_root = f"ui/{channel}/{iir}/"
+            filter_type = all_values[path_root + "filter"]
+            filter_idx = [f.filter_type for f in FILTERS].index(filter_type)
+            kwargs = {
+                param: all_values[path_root + f"{filter_type}/{param}"]
+                for param in FILTERS[filter_idx].parameters
+            }
+            ba = FILTERS[filter_idx].get_coefficients(**kwargs)
+            _iir_widget = self.channel_settings[int(channel)].iir_widgets[int(iir)]
+            _iir_widget.update_transfer_function(ba)
