245291c0204d4f8af8714fb2566635b0c8f7f05b
mollysmith23 <118980653+mollysmith23@users.noreply.github.com>
2026-01-30 16:37:28 +0000

    Added duplicate of dual_iir to update with ff_fb - to be edited

---
+++ stabilizer_ui/target/ff_fb/app.py
@@ -0,0 +1,97 @@
+import argparse
+import asyncio
+import logging
+import sys
+
+from contextlib import suppress
+from PyQt6 import QtWidgets
+from qasync import QEventLoop
+from stabilizer.stream import get_local_ip
+
+from .ui import UiWindow
+from .interface import StabilizerInterface
+from . import topics
+
+from ...stream.thread import StreamThread
+from ...mqtt import NetworkAddress
+from ...utils import fmt_mac, AsyncQueueThreadsafe
+from ...device_db import stabilizer_devices
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser(description="Interface for the Dual-IIR Stabilizer.")
+    parser.add_argument("stabilizer_name",
+                        metavar="DEVICE_NAME",
+                        type=str,
+                        help="Stabilizer name as entered in the device database")
+    parser.add_argument("--stream-port", default=0, type=int)
+    parser.add_argument("--debug", action="store_true", help="Enable debug logging")
+    args = parser.parse_args()
+
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+
+    try:
+        stabilizer = stabilizer_devices[args.stabilizer_name]
+    except KeyError:
+        logger.error(f"Device '{args.stabilizer_name}' not found in device database.")
+        sys.exit(1)
+
+    if stabilizer["application"] != "dual_iir":
+        logger.error(
+            f"Device '{args.stabilizer_name}' is not listed as running dual_iir.")
+        sys.exit(1)
+
+    topics.app_root.name = stabilizer.get("net_id", fmt_mac(stabilizer["mac-address"]))
+    broker_address = stabilizer["broker"]
+
+    # Find out which local IP address we are going to direct the stream to.
+    # Assume the local IP address is the same for the broker and the stabilizer.
+    local_ip = get_local_ip(broker_address.get_ip())
+    requested_stream_target = NetworkAddress(local_ip, args.stream_port)
+
+    app = QtWidgets.QApplication(sys.argv)
+    app.setOrganizationName("Oxford Ion Trap Quantum Computing group")
+    app.setOrganizationDomain("photonic.link")
+    app.setApplicationName("Stabilizer UI")
+
+    with QEventLoop(app) as loop:
+        asyncio.set_event_loop(loop)
+
+        ui = UiWindow(f"Dual_IIR [{args.stabilizer_name}]")
+        ui.show()
+
+        ui.update_comm_status(True,
+                              f"Connecting to MQTT broker at {broker_address.get_ip()}…")
+        stabilizer_interface = StabilizerInterface()
+
+        stream_target_queue = AsyncQueueThreadsafe(maxsize=1)
+        stream_target_queue.put_nowait(requested_stream_target)
+
+        stabilizer_task = loop.create_task(
+            stabilizer_interface.update(ui, broker_address, stream_target_queue))
+
+        stream_thread = StreamThread(
+            ui.update_stream,
+            ui.fftScopeWidget,
+            stream_target_queue,
+            broker_address,
+            loop,
+        )
+        stream_thread.start()
+
+        try:
+            sys.exit(loop.run_forever())
+        finally:
+            stream_thread.close()
+            with suppress(asyncio.CancelledError):
+                stabilizer_task.cancel()
+                loop.run_until_complete(stabilizer_task)
+
+
+if __name__ == "__main__":
+    main()
---
+++ stabilizer_ui/target/ff_fb/interface.py
@@ -0,0 +1,32 @@
+import logging
+from stabilizer import DEFAULT_DUAL_IIR_SAMPLE_PERIOD
+
+from .topics import app_root, StabilizerSettings
+from ...interface import AbstractStabilizerInterface
+
+logger = logging.getLogger(__name__)
+
+
+class StabilizerInterface(AbstractStabilizerInterface):
+    """
+    Shim for controlling `dual-iir` stabilizer over MQTT
+    """
+
+    iir_ch_topic_base = StabilizerSettings.iir_root.path()
+
+    def __init__(self):
+        super().__init__(DEFAULT_DUAL_IIR_SAMPLE_PERIOD, app_root)
+        self.stream_target_topic = StabilizerSettings.stream_target.path(
+            from_app_root=False)
+
+    async def triage_setting_change(self, setting):
+        logger.info(f"Changing setting {setting.path()}': {setting.value}")
+
+        setting_root = setting.app_root()
+        if setting_root.name == "settings":
+            await self.request_settings_change(setting.path(), setting.value)
+        elif setting_root.name == "ui":
+            self.publish_ui_change(setting.path(), setting.value)
+
+            if ui_iir := setting.get_parent_until(lambda x: x.name.startswith("iir")):
+                await self._change_filter_setting(ui_iir)
